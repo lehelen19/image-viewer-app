c374d6d06039378e8a2105668180b3899ae7bf13
Helen <le.helen19@gmail.com>
2021-07-28 14:06:37 -0700

    Finished back button function

--- image-viewer-app.py
+++ image-viewer-app.py
@@ -36,13 +36,26 @@ def forward(image_num):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
-def back():
+def back(image_num):
     global my_label
     global button_forward
     global button_back
 
+    my_label.grid_forget()
+
+    my_label = Label(image=img_list[image_num-1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_num+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_num-1))
+
+    if image_num == 1:
+        button_back = Button(root, text="<<", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+
 # Create buttons for moving pictures
-button_back = Button(root, text="<<", command=back)
+button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_exit = Button(root, text="Exit Program", command=root.quit)
 button_forward = Button(root, text=">>", command=lambda: forward(2))
 
